c31a19a47040b9291e26165d6e7db47abb660fe8
Daniel <dandesantis14@gmail.com>
2021-10-02 17:06:04 -0400

    fixed visibility functionality checks

--- script.js
+++ script.js
@@ -23,25 +23,62 @@ const resumeBox = document.getElementById('resumeBox');
 const contactBoxVisibility = document.getElementById('contactLink');
 const contactBox = document.getElementById('contactBox');
 
+const aboutStyle = window.getComputedStyle(aboutBox);
+const aboutOpacity = aboutStyle.getPropertyValue('opacity');
+
+//const resumeStyle = window.getComputedStyle(resumeBox);
+//const resumeOpacity = resumeStyle.getPropertyValue('opacity');
+
+//const contactStyle = window.getComputedStyle(contactBox);
+//const contactOpacity = contactStyle.getPropertyValue('opacity');
+
 aboutBoxVisibility.addEventListener('click',function(){
-    if(resumeBox.style.opacity === '0' && contactBox.style.opacity === '0'){
+
+    const resumeStyle = window.getComputedStyle(resumeBox);
+    const resumeOpacity = resumeStyle.getPropertyValue('opacity');
+    const contactStyle = window.getComputedStyle(contactBox);
+    const contactOpacity = contactStyle.getPropertyValue('opacity');
+
+    if(resumeOpacity === '0' && contactOpacity === '0'){
         aboutBox.style.opacity = "100%";
     }
     else{
-    resumeBox.style.opacity = 0;
-    contactBox.style.opacity = 0;
-    setTimeout(function(){aboutBox.style.opacity = "100%";},500);
+        resumeBox.style.opacity = 0;
+        contactBox.style.opacity = 0;
+        setTimeout(function(){aboutBox.style.opacity = "100%";},500);
     }
 });
 
 resumeBoxVisibility.addEventListener('click',function(){
-    aboutBox.style.opacity = 0;
-    contactBox.style.opacity = 0;
-    setTimeout(function(){resumeBox.style.opacity = "100%";},500);
+    
+    const aboutStyle = window.getComputedStyle(aboutBox);
+    const aboutOpacity = aboutStyle.getPropertyValue('opacity');
+    const contactStyle = window.getComputedStyle(contactBox);
+    const contactOpacity = contactStyle.getPropertyValue('opacity');
+
+    if(aboutOpacity === '0' && contactOpacity === '0'){
+        resumeBox.style.opacity = "100%";
+    }
+    else{
+        aboutBox.style.opacity = 0;
+        contactBox.style.opacity = 0;
+        setTimeout(function(){resumeBox.style.opacity = "100%";},500);
+    }
 });
 
 contactBoxVisibility.addEventListener('click',function(){
-    aboutBox.style.opacity = 0;
-    resumeBox.style.opacity = 0;
-    setTimeout(function(){contactBox.style.opacity = "100%";},500);
+    
+    const aboutStyle = window.getComputedStyle(aboutBox);
+    const aboutOpacity = aboutStyle.getPropertyValue('opacity');
+    const resumeStyle = window.getComputedStyle(resumeBox);
+    const resumeOpacity = resumeStyle.getPropertyValue('opacity');
+
+    if(aboutOpacity === '0' && resumeOpacity === '0'){
+        contactBox.style.opacity = '100%';
+    }
+    else{    
+        aboutBox.style.opacity = 0;
+        resumeBox.style.opacity = 0;
+        setTimeout(function(){contactBox.style.opacity = "100%";},500);
+    }
 });
