10ca3e51ce10c5b12e29373d20d4e3bf8ff85bd4
Daniel <dandesantis14@gmail.com>
2021-10-02 15:29:10 -0400

    Hiding functionality, some styling

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const menuVisibility = document.getElementById('navMenu');
 document.addEventListener('mouseenter',function(){
     menuVisibility.style.opacity = "100%";
 });
+const menuHide = document.getElementById('navMenu');
+document.addEventListener('mouseleave',function(){
+    menuHide.style.opacity = '0';
+});
 
 /*
 const boxGrab = document.getElementsByClassName('box');
@@ -14,18 +18,25 @@ function visibilityReset(){
 */
 const aboutBoxVisibility = document.getElementById('aboutLink');
 const aboutBox = document.getElementById('aboutBox');
+const resumeBoxVisibility = document.getElementById('resumeLink');
+const resumeBox = document.getElementById('resumeBox');
+const contactBoxVisibility = document.getElementById('contactLink');
+const contactBox = document.getElementById('contactBox');
+
 aboutBoxVisibility.addEventListener('click',function(){
+    resumeBox.style.opacity = 0;
+    contactBox.style.opacity = 0;
     aboutBox.style.opacity = "100%";
 });
 
-const resumeBoxVisibility = document.getElementById('resumeLink');
-const resumeBox = document.getElementById('resumeBox');
 resumeBoxVisibility.addEventListener('click',function(){
+    aboutBox.style.opacity = 0;
+    contactBox.style.opacity = 0;
     resumeBox.style.opacity = "100%";
 });
 
-const contactBoxVisibility = document.getElementById('contactLink');
-const contactBox = document.getElementById('contactBox');
 contactBoxVisibility.addEventListener('click',function(){
+    aboutBox.style.opacity = 0;
+    resumeBox.style.opacity = 0;
     contactBox.style.opacity = "100%";
 });
